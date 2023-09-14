2e735fbf89668bbdf92df10b024ceed762fe4408
Kolodani <danielvinzia@gmail.com>
2023-09-14 16:06:08 -0300

    teoria, procesos, ej007

--- 004-Procesos/ej007/ej007.c
+++ 004-Procesos/ej007/ej007.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main()
 {
     char comando[1000];
+    int *status;
 
     for (;;)
     {
@@ -13,7 +16,9 @@ int main()
         fgets(comando, 1000, stdin);
 
         if (fork())
-            ;
+        {
+            wait(status);
+        }
         else
         {
             execl(comando, comando, NULL);
