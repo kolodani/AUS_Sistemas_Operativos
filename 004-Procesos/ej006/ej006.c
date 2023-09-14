dea8e07dc1db487537f216cca09a1895aeb27f38
Kolodani <danielvinzia@gmail.com>
2023-09-14 15:53:44 -0300

    teoria, procesos, ej006

---
+++ 004-Procesos/ej006/ej006.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main()
+{
+    char comando[1000];
+
+    for (;;)
+    {
+        printf("$ ");
+        gets(comando);
+
+        if (fork())
+            ;
+        else
+        {
+            execl(comando, comando, NULL);
+        }
+    }
+    return 0;
+}
