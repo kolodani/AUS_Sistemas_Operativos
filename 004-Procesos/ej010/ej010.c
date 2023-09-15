cd768dc113f261c3432850a4b2da9f734fe13042
Kolodani <danielvinzia@gmail.com>
2023-09-15 15:19:34 -0300

    teoria, procesos, ej010

---
+++ 004-Procesos/ej010/ej010.c
@@ -0,0 +1,46 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid_hijo;
+    int *status_hijo;
+
+    pid_hijo = fork();
+
+    if (pid_hijo)
+    {
+        /* Padre */
+        printf("Mi hijo es el proceso %d, espero...", pid_hijo);
+        wait(status_hijo);
+        /* Determinar el motivo de la terminacion */
+        if (WIFEXITED(*status_hijo))
+        {
+            printf("\nTerminacion normal del proceso hijo con exit code %d.\n", WEXITSTATUS(*status_hijo));
+        }
+        if (WIFSIGNALED(*status_hijo))
+        {
+            int signal_id = WTERMSIG(*status_hijo);
+            char *signal_name = strsignal(signal_id);
+            printf("\nTerminacion debido a la señal %d (%s).\n", signal_id, signal_name);
+        }
+    }
+    else
+    {
+        /* Hijo */
+        int i = 0;
+        sleep(5);
+        while (i++ < 120)
+        {
+            printf(stdout, "%d - ", i++);
+            sleep(1);
+        }
+        printf("Hijo termina normalmente\n");
+        _exit(status_hijo);
+    }
+    return 0;
+}
