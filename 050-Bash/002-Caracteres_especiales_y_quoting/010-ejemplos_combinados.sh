07da65532846a545f2752dedddbb49fd80e347c9
Kolodani <danielvinzia@gmail.com>
2023-09-16 18:59:09 -0300

    BASH, teoria, bash, caracteres especiales y quoting, 010

---
+++ 050-Bash/002-Caracteres_especiales_y_quoting/010-ejemplos_combinados.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# EJEMPLOS COMBINADOS
+#echo <-$1250.**>; (update?) [y|n]
+echo \<-\$1250.\*\*\>\; \(update\?\) \[y\|n\]
+echo '<-$1250.**>; (update?) [y|n]'
+
+echo La variable '$UID' contiene el valor --\> "$UID"
+
+#echo It's <party> time!
