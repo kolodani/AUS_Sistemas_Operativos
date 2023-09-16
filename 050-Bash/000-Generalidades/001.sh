045ae184fdbea4dedef3d7e7f77c187b8cc47041
Kolodani <danielvinzia@gmail.com>
2023-09-16 15:03:47 -0300

    BASH, teoria, bash, generalidades, 001

---
+++ 050-Bash/000-Generalidades/001.sh
@@ -0,0 +1,8 @@
+# los comentarios se hacen en con el numeral
+
+# comandos:
+# la sintaxis es:
+# COMANDO  OPCIONES  ARGUMENTOS
+
+# por lo general vamos a hacer SCRIPTS
+# que son listas de comandos para no escribir todo de una en las lineas de bash
