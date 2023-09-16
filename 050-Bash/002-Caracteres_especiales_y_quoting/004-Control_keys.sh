ca2240f4eca25660043e782c81ced626f8c43e94
Kolodani <danielvinzia@gmail.com>
2023-09-16 16:36:46 -0300

    BASH, teoria, bash, caracteres especiales y quoting, 004

---
+++ 050-Bash/002-Caracteres_especiales_y_quoting/004-Control_keys.sh
@@ -0,0 +1,15 @@
+# CONTROL KEYS                DESCRIPCION
+# CTRL-C                      Detiene el comando actual (envia SIGINT)
+# CTRL-D                      Finaliza la entrada estandar (EOF)
+# CTRL-\                      Detiene el comando actual (si no funciona CTRL-C, envia SIGQUIT)
+# CTRL-S                      Detiene la salida por pantalla (XOFF)
+# CTRL-Q                      Reinicia la salida por pantalla (XON)
+# DEL or CTRL-?               Borra ultimo caracter
+# CTRL-W                      Borra la ultima palabra de la linea de comandos
+# CTRL-U                      Borra la linea de comandos entera
+# CTRL-Z                      Suspende el proceso actual (envia SIGTSTP)
+# CTRL-A                      Cursor al principio de la linea de comandos
+# CTRL-E                      Cursor al final de la linea de comandos
+# CTRL-R                      Busqueda recursiva de comandos
+# CTRL-M                      Equivalente a ENTER (retorno de carro)
+# CTRL-h                      Borra el caracter anterior (backspace)
