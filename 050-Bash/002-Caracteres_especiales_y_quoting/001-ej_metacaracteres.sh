2f7d3731124300da0528e02d5cab7770e17ef5f7
Kolodani <danielvinzia@gmail.com>
2023-09-16 16:09:23 -0300

    BASH, teoria, bash, caracteres especiales y quoting, 001

---
+++ 050-Bash/002-Caracteres_especiales_y_quoting/001-ej_metacaracteres.sh
@@ -0,0 +1,6 @@
+# Ejemplos
+# cd ~/libros                              # Cambia al directorio libros
+# rm *.bck                                 # Borra todos los archivos con extensión .bck
+# find / -name a* &                        # Busca todos los archivos que empiecen por a
+# echo Fecha y hora actual: `date`         # Muestra la fecha y hora actual
+# echo Hay `wc -l /etc/passwd | awk '{print $1}'` usuarios  # Muestra el número de usuarios
