a8efe3ef95c7b7784888424e66be32863e130c2c
Kolodani <danielvinzia@gmail.com>
2023-09-16 15:54:21 -0300

    BASH, teoria, bash, introduccion, 003

---
+++ 050-Bash/001-Introduccion/003-ej_alias.sh
@@ -0,0 +1,16 @@
+# ALIAS
+# ejemplos de alias:
+#
+# alias [name=command] // sin espacions entre el signo =
+# ejemplo:
+# alias lf='ls -F'
+# alias revsort='ls [0-9]* | sort -v'
+#
+# NO se permiten alias recursivos
+# alias ls='ls -la'  // MAL
+#
+# Porque usar alias?
+# Brevedad:        (ls=ls -lha)
+# Costumbre:       (dir=ls)
+# Proteccion:      (rm=rm -i)
+# personalizacion: (moer=more)
