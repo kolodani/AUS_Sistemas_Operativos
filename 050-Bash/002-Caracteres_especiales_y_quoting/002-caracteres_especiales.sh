72033e57b198ef06aea66da4b19a0c67c90aad2d
Kolodani <danielvinzia@gmail.com>
2023-09-16 16:20:15 -0300

    BASH, teoria, bash, caracteres especiales y quoting, 002

---
+++ 050-Bash/002-Caracteres_especiales_y_quoting/002-caracteres_especiales.sh
@@ -0,0 +1,22 @@
+# CARACTER          SIGNIFICADO
+#    ~              Home del usuario o directorio home
+#    `              Sustitución de comando
+#    #              Comentario
+#    $              Valor de variable
+#    &              Ejecución en segundo plano o Trabajo en background
+#    *              Estrella de Kleene (expresiones regulares)
+#    (              Inicio de subshell
+#    )              Fin de subshell
+#    |              Pipe
+#    [              Inicio de lista de caracteres (expresiones regulares)
+#    ]              Fin de lista de caracteres (expresiones regulares)
+#    {              Inicio de bloque de comandos
+#    }              Fin de bloque de comandos
+#    ;              Secuencializar comandos o fin de comando
+#    '              Comilla simple (Strong quote)
+#    "              Comillas dobles (Weak quote)
+#    <              Redirección de entrada
+#    >              Redirección de salida
+#    /              Separador de directorios en pathname
+#    ?              Reemplazo de una caracter (expresiones regulares)
+#    !              Negación de pipeline
