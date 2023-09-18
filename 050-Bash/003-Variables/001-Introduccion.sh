a3b1574282dae9c643c9301e2e35264b493dbf8d
Kolodani <danielvinzia@gmail.com>
2023-09-18 17:03:49 -0300

    BASH, teoria, bash, variables, 001

---
+++ 050-Bash/003-Variables/001-Introduccion.sh
@@ -0,0 +1,13 @@
+# VARIABLES
+
+# Una variable tiene un nombre y un valor ($nombre)
+# - Creacion:         Var1=10        # sin espacios!
+# - Asignacion:       Var2=$Var1
+# - Borrado:          unset Var1
+
+# Guardar comandos
+# - Con acentos graves:
+# $ lista_de_archivos=`ls`
+# - Con (...): anidalbe
+# $ lista_de_archivos=$(ls)
+# $ lista_de_archivos=$(ls $(cat directorios.txt))
