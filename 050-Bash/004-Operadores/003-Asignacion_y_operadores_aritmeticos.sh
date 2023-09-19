a00d9e4e0d8c11f2f2e41aaf5ffd1293b0cc0cfe
Kolodani <danielvinzia@gmail.com>
2023-09-19 15:50:50 -0300

    BASH, teoria, bash, Operadores, 003

---
+++ 050-Bash/004-Operadores/003-Asignacion_y_operadores_aritmeticos.sh
@@ -0,0 +1,21 @@
+# ASIGNACION Y OPERADORES ARITMETICOS
+
+# variable=asignacion
+# inicializa o cambia el valor de una variable
+# Funciona tanto pata enteros como para cadenas
+# Advertencia: no confundir con el operador de comparacion =
+var=27
+animal=tigre     # Sin espacios entre el signo =
+
+# Operadores aritmeticos
+# =   suma
+# -   resta
+# *   producto
+# /   cociente
+# **  exponenciacion
+# %   modulo o mod
+# +=  mas-igual
+# -=  menos-igual
+# *=  por-igual
+# /=  dividido-igual
+# %=  modulo-igual
