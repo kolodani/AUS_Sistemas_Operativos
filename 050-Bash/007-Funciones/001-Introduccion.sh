d5c01910a8546dad4c70b5d9e17a4e96924c27ab
Kolodani <danielvinzia@gmail.com>
2023-09-22 14:39:07 -0300

    BASH, teoria, bash, Funciones, 001

---
+++ 050-Bash/007-Funciones/001-Introduccion.sh
@@ -0,0 +1,19 @@
+# INTRODUCCION A FUNCIONES
+
+# Sintaxis permitidas:
+# funtion nombre_funcion {
+#     comando...
+# }
+# ----------------------
+# nombre_funcion () {
+#     comando
+# }
+
+# Ejemplo:
+#!/bin/bash
+saludo () {
+    echo -n "Hola "
+}
+
+saludo
+echo "Mundo"
