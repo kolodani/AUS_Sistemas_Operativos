ed6737a70351c37567783a27186c77bb5fb2d06d
Kolodani <danielvinzia@gmail.com>
2023-09-22 15:49:53 -0300

    BASH, teoria, bash, Entrada y salida redirecciones, 005

---
+++ 050-Bash/008-Entrada_y_Salida_Redirecciones/005-Ejemplo.sh
@@ -0,0 +1,16 @@
+# EJEMPLO
+#cat >> .bashrc
+#alias ll='ls -lha'
+#alias rm='rm -i'
+#^D
+
+# cat < archivo1 > archivo2
+# similar a cp archivo1 archivo2
+
+# lpr << MIS_URLS
+# http://sun.sunsolve.com
+# http://docs.sun.com
+# http://www.sunfreeware.com
+# MIS_URLS
+
+echo "Mensaje" 1>&2
