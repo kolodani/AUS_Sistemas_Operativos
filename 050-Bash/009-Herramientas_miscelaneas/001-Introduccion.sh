19c88f27fc29a64c0b5c5ace45db9d15921e03d6
Kolodani <danielvinzia@gmail.com>
2023-09-22 16:31:35 -0300

    BASH, teoria, bash, Herramientas miscelaneas, 001

---
+++ 050-Bash/009-Herramientas_miscelaneas/001-Introduccion.sh
@@ -0,0 +1,17 @@
+# INTRODUCCION
+# Herramientas utiles para la confeccion de scripts
+
+# comnados propios del shell (built-in)
+#    eval
+#    :  (dos puntos)
+#    type
+
+# comandos externos
+#    sleep
+#    find
+#    xargs
+#    expr
+#    bc
+
+# los comandos built-in se ejecutan de manera mas eficiente que los externos
+# pues no existen accesos a disco para su ejecucion
