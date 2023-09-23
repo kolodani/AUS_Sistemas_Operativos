3d98b8d3cf9c1457fe97b868b7f3c494f0c82558
Kolodani <danielvinzia@gmail.com>
2023-09-23 19:31:30 -0300

    BASH, Practica02, ejercicio 001 a

---
+++ 103-Practica02/ejercicio001/001-a.sh
@@ -0,0 +1,16 @@
+# Ejercicio 001: Comando tac
+# a) Interpretar la página de manual del comando cat, realizar pruebas del comando
+
+echo "cat - concatena archivos e imprime en la salida estándar"
+
+# Sintaxis
+echo "Sintaxis: cat [OPCIÓN]... [ARCHIVO]..."
+
+echo "Comando: cat ejemplo.txt"
+cat ejemplo.txt
+
+echo "Comando: cat -n ejemplo.txt"
+cat -n ejemplo.txt
+
+echo "Comando: cat -b ejemplo.txt"
+cat -b ejemplo.txt
