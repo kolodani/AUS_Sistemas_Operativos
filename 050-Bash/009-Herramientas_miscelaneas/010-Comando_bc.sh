baf22cd01e27e52af7ec485e85660b9c238f31ca
Kolodani <danielvinzia@gmail.com>
2023-09-23 16:38:18 -0300

    BASH, teoria, bash, Herramientas miscelaneas, 010

---
+++ 050-Bash/009-Herramientas_miscelaneas/010-Comando_bc.sh
@@ -0,0 +1,20 @@
+# COMANDO BC
+
+# Pontente utilida para realizar calculos numericos no restringida a los enteros
+bc
+scale=4
+8/3
+2.6666
+2.5 * 4.1/6.9        # Los espacios son opcionales
+1.4855
+quit
+
+# Operadores:
+#    +     suma
+#    -     resta
+#    *     multiplicacion (operador quoted)
+#    /     division entera
+#    %     resto de la division
+#    ^     Exponenciacion
+
+# Opera de manera precisa con numeros de cualquier tamaño
