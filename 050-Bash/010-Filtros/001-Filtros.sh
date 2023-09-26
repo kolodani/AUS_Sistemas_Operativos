21cbe174bd97c5fa0c8bf38d7b53c9e84ff4b6c8
Kolodani <danielvinzia@gmail.com>
2023-09-26 18:57:14 -0300

    BASH, teoria, bash, Filtros, 001

---
+++ 050-Bash/010-Filtros/001-Filtros.sh
@@ -0,0 +1,15 @@
+# FILTROS
+
+# Comandos que leen alguna entrada, realizan una transformacion de
+# la misma o calculan valores mediante esta y escriben alguna salida
+
+# Comandos de filtrado tipico
+# - sort: Ordena lineas de archivos de texto (sort -k, sorf -f)
+# - tr: Traduce o elimina caracteres (trabaja con STDIN)
+# - uniq: Elimina lineas duplicadas en un archivo ordenado (sort -u)
+# - head: Muestra las primeras lineas de un fichero
+# - tail: Muestra las ultimas lineas de un fichero
+# - wc: estadisticas simples para archivos de testo
+# - rev: invierte cada line de un archivo y la envia a STDOUT
+# - cat: Concatena el contenido de uno o mas archivos (cat -n)
+# - cut: Imprime partes seleccionadas de los archivos de entrada (cut -b)
