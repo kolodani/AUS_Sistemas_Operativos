e769c7f86c2adf2819fe46088e543bebb54628ee
Kolodani <danielvinzia@gmail.com>
2023-09-27 11:29:04 -0300

    BASH, teoria, bash, Lenguaje awk, 009

---
+++ 050-Bash/011-Lenguaje_awk/009-Acciones.sh
@@ -0,0 +1,19 @@
+# ACCIONES
+
+# Una accion = una o mas sentencias awk y se encierra entre llaves {}
+# Las sentencias son separadas por caracteres nueva-linea o ; (punto y coma)
+
+# Tipos de sentencias
+# - Expresiones
+#    Invocacion de funciones, asignacion de variables, variables
+#    y funciones built-in
+# - Sentencias de control
+#    Similares a las del lenguaje C: if, for, while, do-while, break, continue
+# - Sentencias compuestas
+#    Consisten en una o mas sentencias encerradas entre llaves, bloques
+# - Control de entrada
+#    getline y next
+# - Sentencias de salida
+#    print y printf
+# - Sentencia de borrado
+#    para eliminar elementos de una array
