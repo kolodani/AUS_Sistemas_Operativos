38c2a9ba39c7f0c493fe6085a370357a5da5a305
Kolodani <danielvinzia@gmail.com>
2023-10-09 15:51:08 -0300

    BASH, Practica POSIX, ejercicio03

---
+++ 102-Manual_de_practicas/ejercicio03/forkprog2.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main()
+{
+    int i = 0; // cambios pregunta b)
+    int j;
+    pid_t rf;
+
+    rf = fork();
+    switch (rf) {
+    case -1:
+        printf ("\nNo he podido crear el proceso hijo");
+        break;
+    case 0:
+        // i = 0;     // cambios pregunta b)
+        printf ("\nSoy el hijo, mi PID es %d y mi variable i (inicialmente a %d) crece de 1 en 1", getpid(), i);
+        for (j = 0; j < 5; j++)
+        {
+            i ++;
+            // i ++;    // cambios pregunta b)
+            printf("\nSoy el hijo, mi variable i es %d", i);
+        };
+        break;
+    default:
+        // i = 1;      // cambios pregunta b)
+        printf ("\nSoy el padre, mi PID es %d y mi variable i (inicialmente a %d) crece de 2 en 2", getpid(), i);
+        for (j = 0; j < 5; j++)
+        {
+            i += 2;    // cambios pregunta b)
+            // i++;    // cambios pregunta b)
+            printf("\nSoy el padre, mi variable i es %d", i);
+        };
+    };
+    printf ("\nFinal de ejecucion de %d \n", getpid());
+    exit(0);
+}
