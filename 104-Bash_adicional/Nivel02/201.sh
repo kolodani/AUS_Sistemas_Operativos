94010d747a77a8410e1914dd7313ad74d21b7683
Kolodani <danielvinzia@gmail.com>
2023-10-29 16:14:42 -0300

    reinstalando manjaro

---
+++ 104-Bash_adicional/Nivel02/201.sh
@@ -0,0 +1,3 @@
+# Crear un script que reciba dos rutas a archivos de texto y que indique si estos archivos
+# tienen la misma cantidad de líneas. Validar que los archivos existen y se pueden leer.
+
